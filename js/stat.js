28d7ec3d1f7c50c839c8b7a317a7128d4c816cfb
annacomar <volokhova.anna@gmail.com>
2020-10-05 11:17:22 +0200

    Вопрос к наставнику

--- js/stat.js
+++ js/stat.js
@@ -2,17 +2,16 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+
+
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
-var FONT_GAP = 15; //Question 1: правильное ли это значение и откуда оно берётся? Шрифт же 16px.
-//var FONT_COLOR
-// var BAR__COLOR
-// Question 2: нужно ли задавать эти переменные?
-//Иначе как тогда каждый раз задавать разный цвет то тексту, то колонке?
-var BAR__HEIGHT = 150;
+/*var GAP = 10;
+var FONT_GAP = 15;
+var HIST_HEIGHT = 150;
+var BAR_HEIGHT = HIST_HEIGHT - TEXT_HEIGHT - FONT_GAP - TEXT_HEIGHT - FONT_GAP;
 var BAR_WIDTH = 40;
-var BAR_DISTANCE = 50;
+var BAR_DISTANCE = 50;*/
 
 var renderCloud = function(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -38,20 +37,21 @@ window.renderStatistics = function (ctx) {
   ctx.fillStyle = '#000';
   ctx.fillText('4025', 235, 75);
   ctx.fillText('Кекс', 235, 255);
-  ctx.fillStyle = '#00ffff'; // Question 3: не очень понятно, каким образом задавать синий цвет случайной прозрачности.
+  ctx.fillStyle = '#0000FF';
+  //ctx.fillStyle = `hsl(240, ` + randomColor + `%, 50%)`;
   ctx.fillRect(235, 100, 40, 130);
 
 
   ctx.fillStyle = '#000';
   ctx.fillText('1244', 325, 165);
   ctx.fillText('Катя', 325, 255);
-  ctx.fillStyle = '#00ffff';
+  //ctx.fillStyle = `hsl(240, ` + randomColor + `%, 50%)`;
   ctx.fillRect(325, 190, 40, 40);
 
 
   ctx.fillStyle = '#000';
   ctx.fillText('1339', 415, 165);
   ctx.fillText('Игорь', 415, 255);
-  ctx.fillStyle = '#00ffff';
+  //ctx.fillStyle = `hsl(240, ` + randomColor + `%, 50%)`;
   ctx.fillRect(415, 180, 40, 50);
-  };
+};
